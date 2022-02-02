70128c0e09872a517cef13ebad7159367135a467
RankSS <benyang4869@gmail.com>
2022-02-02 01:57:06 -0500

    Pairwise Alignment now correctly returns all possible alignments

--- PairwiseAlignment.py
+++ PairwiseAlignment.py
@@ -7,7 +7,7 @@ from copy import deepcopy
 
 class PairwiseAlignment:
     """Global/Local Pairwise Alignment
-    
+
     Attributes:
         hseq (Sequence): Horizontal sequence
         vseq (Sequence): Vertical sequence
@@ -15,7 +15,7 @@ class PairwiseAlignment:
         dp_array (List): Contains final scoring of alignment
         direction (List): Contains direction per cell
         alignment (List): Contains all optimal alignments
-        
+
     Methods:
         Global (None): Performs Needleman-Wunsch with affine gap penalty
         Local (None): Performs Smith-Waterman with affine gap penalty
@@ -28,6 +28,7 @@ class PairwiseAlignment:
         self.score = score
         self.dp_array = None
         self.direction = None
+        self.paths = None
         self.alignment = None
         return
 
@@ -72,8 +73,8 @@ class PairwiseAlignment:
                 matrices["v_gap"][j][i] = max(matrices["v_gap"][j-1][i] + extend,
                                          self.dp_array[j-1][i] + exist + extend)
                 matrices["match"][j][i] = self.dp_array[j-1][i-1] + matrix[self.vseq[j-1]][self.hseq[i-1]]
-                self.dp_array[j][i] = max(matrices["v_gap"][j][i],
-                                          matrices["h_gap"][j][i],
+                self.dp_array[j][i] = max(matrices["h_gap"][j][i],
+                                          matrices["v_gap"][j][i],
                                           matrices["match"][j][i])
                 # Direction
                 if self.dp_array[j][i] == matrices["h_gap"][j][i]:
@@ -84,10 +85,69 @@ class PairwiseAlignment:
                     self.direction[j][i] += "D"
 
         # Find alignment(s)
-        # TODO
-        
+        self.paths = []
+        self.__Paths("", 0, hlen-1, vlen-1)
+        self.__Alignments()
+        print(f"Score: {self.dp_array[vlen-1][hlen-1]}")
         return
 
+    def __Paths(self, temp_path: list, iteration: int, i: int, j: int, i_min=0, j_min=0):
+        """This function was rough to code...
+
+        Args:
+            temp_path (list): Temporary path
+            i (int): Optimal horizontal index in dp_array
+            j (int): Optimal vertical index in dp_array
+            i_min (int, optional): Horizontal index of where path end. Defaults to 0.
+            j_min (int, optional): Vertical index of where path end. Defaults to 0.
+        """
+        dir_dict = {
+            "L": [-1, 0],
+            "U": [0, -1],
+            "D": [-1, -1]
+        }
+
+        # Perform depth first walk while obtaining each path
+        if i > i_min or j > j_min:
+            curr = self.direction[j][i]
+            for direction in curr:
+                self.__Paths(temp_path + direction, iteration + 1,
+                            i + dir_dict[direction][0],
+                            j + dir_dict[direction][1])
+        else:
+            self.paths.append(deepcopy(temp_path))
+        return
+
+    def __Alignments(self):
+        """Prints alignment
+        """
+        for i in range(len(self.paths)):
+            path = self.paths[i][::-1]
+            hmatch = ""
+            vmatch = ""
+            hcounter = 0
+            vcounter = 0
+            for direction in path:
+                if direction == "D":
+                    hmatch += self.hseq[hcounter]
+                    vmatch += self.vseq[vcounter]
+                    hcounter += 1
+                    vcounter += 1
+                if direction == "L":
+                    hmatch += self.hseq[hcounter]
+                    vmatch += "-"
+                    hcounter += 1
+                if direction == "U":
+                    hmatch += "-"
+                    vmatch += self.vseq[vcounter]
+                    vcounter += 1
+
+            print(f"Alignment {i+1}:")
+            print(hmatch)
+            print(vmatch)
+            
+        return
+    
     def Local(self):
         """Performs Smith-Waterman for sequence pair with affine gap penalty
         """
@@ -136,9 +196,9 @@ class PairwiseAlignment:
         print(self.score)
         # TODO
 
-score = Score(MatrixConstants.EXAMPLE_MATRIX, BlockConstants.NUCLEOTIDES, 0, 2)
+score = Score(MatrixConstants.EXAMPLE_MATRIX, BlockConstants.NUCLEOTIDES, 0, -1)
 print(score)
-DNASeq1 = DNASequence("CAAGAC")
-DNASeq2 = DNASequence("GAAC")
+DNASeq1 = DNASequence("GTCGACGCA")
+DNASeq2 = DNASequence("GATTACA")
 alignment = PairwiseAlignment(DNASeq1, DNASeq2, score)
 alignment.Global()
